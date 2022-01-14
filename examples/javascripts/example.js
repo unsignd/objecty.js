aa8fef2659067207f9b9f4822c7c474ebed5cd81
Yejun Kim <unsigndid@gmail.com>
2022-01-14 10:16:02 +0900

    🎨 combine clearRect with rect

--- examples/javascripts/example.js
+++ examples/javascripts/example.js
@@ -1,4 +1,4 @@
-import { Arc, Polygon, Renderer } from "../../src/objecty.js";
+import { Arc, Polygon, Rect, Renderer } from "../../src/objecty.js";
 
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
@@ -7,19 +7,33 @@ const square = new Polygon([
   {x: 10, y: 10},
   {x: 50, y: 10},
   {x: 50, y: 50},
-  {x: 10, y: 50}
+  {x: 10, y: 80}
 ], {
-  color: '#faf',
-  fillVisible: true
+  fillColor: '#faf'
 });
 
-const circle = new Arc(10, 10, 10, 0, Math.PI * 2, {
+const circle = new Arc(10, 10, 30, 0, Math.PI * 2, {
   strokeColor: 'red',
-  lineWidth: 2,
+  lineWidth: 10,
   fillVisible: false,
   strokeVisible: true
 });
 
-const myRenderer = new Renderer(square, circle);
+const rect = new Rect(100, 150, 50, 80, {
+  fillColor: '#123456'
+});
+
+const clearRect = new Rect(20, 20, 50, 50, {
+  doClear: true
+});
+
+const myRenderer = new Renderer(square, circle, rect, clearRect);
 
 myRenderer.render(ctx);
+
+setInterval(() => {
+  circle.radius += 1;
+  circle.x += 1;
+
+  myRenderer.render(ctx);
+}, 1);
--- src/objecty.js
+++ src/objecty.js
@@ -1,7 +1,7 @@
 export class Polygon {
   constructor(path, option) {
     this.path = path;
-    this.color = (typeof option.color !== 'undefined') ? option.color : '#000';
+    this.color = (typeof option.fillColor !== 'undefined') ? option.fillColor : '#000';
     this.strokeColor = (typeof option.strokeColor !== 'undefined') ? option.strokeColor : '#000';
     this.lineWidth = (typeof option.lineWidth !== 'undefined') ? option.lineWidth : 1;
     this.fillVisible = (typeof option.fillVisible !== 'undefined') ? option.fillVisible : true;
@@ -21,7 +21,7 @@ export class Arc {
     this.startAngle = startAngle;
     this.endAngle = endAngle;
     this.clockwise = (typeof option.clockwise !== 'undefined') ? option.clockwise : false;
-    this.color = (typeof option.color !== 'undefined') ? option.color : '#000';
+    this.color = (typeof option.fillColor !== 'undefined') ? option.fillColor : '#000';
     this.strokeColor = (typeof option.strokeColor !== 'undefined') ? option.strokeColor : '#000';
     this.lineWidth = (typeof option.lineWidth !== 'undefined') ? option.lineWidth : 1;
     this.fillVisible = (typeof option.fillVisible !== 'undefined') ? option.fillVisible : true;
@@ -33,6 +33,28 @@ export class Arc {
   }
 }
 
+export class Rect {
+  constructor(x, y, width, height, option) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.doClear = (typeof option.doClear !== 'undefined') ? option.doClear : false;
+    this.color = (typeof option.fillColor !== 'undefined') ? option.fillColor : '#000';
+    this.strokeColor = (typeof option.strokeColor !== 'undefined') ? option.strokeColor : '#000';
+    this.lineWidth = (typeof option.lineWidth !== 'undefined') ? option.lineWidth : 1;
+    this.fillVisible = (typeof option.fillVisible !== 'undefined') ? option.fillVisible : true;
+    this.strokeVisible = (typeof option.strokeVisible !== 'undefined') ? option.strokeVisible : false;
+  }
+
+  checkType() {
+    if (this.doClear)
+      return 3;
+    else
+      return 2;
+  }
+}
+
 export class Renderer {
   constructor(...objects) {
     this.objs = objects;
@@ -40,6 +62,7 @@ export class Renderer {
 
   render(context) {
     this.ctx = context;
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
     this.objs.forEach(obj => {
       this.ctx.beginPath();
@@ -59,23 +82,34 @@ export class Renderer {
               this.ctx.lineTo(path.x, path.y);
             }
           });
+
+          if (obj.fillVisible)
+            this.ctx.fill();
+          if (obj.strokeVisible)
+            this.ctx.stroke();
           break;
         case 1:
           this.ctx.arc(obj.x, obj.y, obj.radius, obj.startAngle, obj.endAngle, obj.clockwise);
+
+          if (obj.fillVisible)
+            this.ctx.fill();
+          if (obj.strokeVisible)
+            this.ctx.stroke();
+          break;
+        case 2:
+          if (obj.fillVisible)
+            this.ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
+          if (obj.strokeVisible)
+            this.ctx.strokeRect(obj.x, obj.y, obj.width, obj.height);
+          break;
+        case 3:
+          this.ctx.clearRect(obj.x, obj.y, obj.width, obj.height);
           break;
         default:
           break;
       }
       
       this.ctx.closePath();
-      
-      if (obj.fillVisible) {
-        this.ctx.fill();
-      }
-      if (obj.strokeVisible) {
-        this.ctx.stroke();
-      }
     });
   }
-  
 }
